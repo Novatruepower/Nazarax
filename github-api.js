28f5bb648636f4faaf63b453da75dcdd3dc91731
Novatruepower <patatotrape3@gmail.com>
2025-12-07 12:39:42 -0500

    fix

--- github-api.js
+++ github-api.js
@@ -1,28 +1,27 @@
-function redirectToEndPoint(endpoint) {
-    const url = `${this.startEndpoint}${endpoint}`;
-    const refresh = document.createElement("meta");
-    refresh.httpEquiv = "refresh";
-    refresh.content= `0; URL=${url}`;
-    const canonical = document.createElement("link");
-    canonical.rel = "canonical";
-    canonical.href = url;
-    document.head.appendChild(refresh);
-    document.head.appendChild(canonical);
-}
-
-
 export const API = {
     startEndpoint: `${window.location.href.split(".github.io/", 2)[0]}.github.io/`,
 
+    redirectToUrl: function(endpoint, startEndpoint = "") {
+        const url = `${startEndpoint}${endpoint}`;
+        const refresh = document.createElement("meta");
+        refresh.httpEquiv = "refresh";
+        refresh.content= `0; URL=${url}`;
+        const canonical = document.createElement("link");
+        canonical.rel = "canonical";
+        canonical.href = url;
+        document.head.appendChild(refresh);
+        document.head.appendChild(canonical);
+    },
+
     redirectToEndPoint: function(endpoint) {
-        redirectToEndPoint(`/${endpoint}/`);
+        this.redirectToUrl(`/${endpoint}/`, this.startEndpoint);
     },
 
     redirectToFolderEndPoint: function() {
         if (window.location.pathname.length > 1) {
             const pathsnames = window.location.pathname.substring(1, window.location.pathname.length).split("/", 2);
             if (pathsnames.length >= 2) {
-                redirectToEndPoint(`${pathsnames[0]}${pathsnames[1]}`);
+                this.redirectToUrl(`${pathsnames[0]}${pathsnames[1]}`, this.startEndpoint);
             }
         }
     }
