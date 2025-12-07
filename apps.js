c502130b42f0f3abb2b4d110879d9619cf1312cf
Novatruepower <patatotrape3@gmail.com>
2025-12-07 03:30:59 -0500

    test

---
+++ apps.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const iframe = document.getElementById('externalPageFrame');
+    iframe.src = 'https://novatruepower.github.io/NazaraxSheet/'; // Replace with the URL of the external page
+});
