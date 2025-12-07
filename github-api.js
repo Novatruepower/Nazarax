cf59425389bb24ee4f09b68135f87a7662f5f99e
Novatruepower <patatotrape3@gmail.com>
2025-12-07 12:34:41 -0500

    test

--- github-api.js
+++ github-api.js
@@ -1,20 +1,28 @@
-export const API = {
-    endpoint: `${window.location.href.split(".github.io/", 2)[0]}.github.io/`,
+function redirectToEndPoint(endpoint) {
+    const url = `${this.startEndpoint}${endpoint}`;
+    const refresh = document.createElement("meta");
+    refresh.httpEquiv = "refresh";
+    refresh.content= `0; URL=${url}`;
+    const canonical = document.createElement("link");
+    canonical.rel = "canonical";
+    canonical.href = url;
+    document.head.appendChild(refresh);
+    document.head.appendChild(canonical);
+}
 
-    redirectToEndPoint: function() {
-        console.log(this.endpoint);
+
+export const API = {
+    startEndpoint: `${window.location.href.split(".github.io/", 2)[0]}.github.io/`,
+
+    redirectToEndPoint: function(endpoint) {
+        redirectToEndPoint(`/${endpoint}/`);
+    },
+
+    redirectToFolderEndPoint: function() {
         if (window.location.pathname.length > 1) {
             const pathsnames = window.location.pathname.substring(1, window.location.pathname.length).split("/", 2);
             if (pathsnames.length >= 2) {
-                const url = `${this.endpoint}${pathsnames[0]}${pathsnames[1]}`;
-                const refresh = document.createElement("meta");
-                refresh.httpEquiv = "refresh";
-                refresh.content= `0; URL=${url}`;
-                const canonical = document.createElement("link");
-                canonical.rel = "canonical";
-                canonical.href = url;
-                document.head.appendChild(refresh);
-                document.head.appendChild(canonical);
+                redirectToEndPoint(`${pathsnames[0]}${pathsnames[1]}`);
             }
         }
     }
