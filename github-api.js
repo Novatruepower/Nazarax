6561e973c2161cc20826d75580f823982b14af0e
Novatruepower <patatotrape3@gmail.com>
2025-12-07 11:24:40 -0500

    debug

--- github-api.js
+++ github-api.js
@@ -3,14 +3,6 @@ export const API = {
     startEndpoint: window.location.href.split(".github.io/", 2)[0] + ".github.io/",
 
     redirectToEndPoint: function(endpoint) {
-        const refresh = document.createElement("meta");
-        refresh.httpEquiv = "refresh";
-        const url = `${this.startEndpoint}/${endpoint}/`;
-        refresh.content= `0; URL=${url}`;
-        const canonical = document.createElement("link");
-        canonical.rel = "canonical";
-        canonical.href = url;
-        document.head.appendChild(refresh);
-        document.head.appendChild(canonical);
+        console.log(window.location.hostname);
     }
 }
