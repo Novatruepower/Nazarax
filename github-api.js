67cbcbcfcddfe06e5f4f5499e62e6cbbef361db4
Novatruepower <patatotrape3@gmail.com>
2025-12-07 11:35:27 -0500

    fix

--- github-api.js
+++ github-api.js
@@ -1,11 +1,11 @@
 
 export const API = {
-    startEndpoint: window.location.href.split(".github.io/", 2)[0] + ".github.io/",
+    startEndpoint: window.location.href.split(".github.io/", 2)[0] + window.location.pathname + ".github.io/",
 
     redirectToEndPoint: function(endpoint) {
         const refresh = document.createElement("meta");
         refresh.httpEquiv = "refresh";
-        const url = `${this.startEndpoint}/${window.location.pathname.split("./", 2)[0]}/${endpoint}/`;
+        const url = `${this.startEndpoint}/${endpoint}/`;
         refresh.content= `0; URL=${url}`;
         const canonical = document.createElement("link");
         canonical.rel = "canonical";
